6300c629fea1020ea57c3c42f0aff09954bc47bd
Konstantin Snurnicyn <k.snurnicyn@esterna.eu>
2023-11-30 13:00:13 +0100

    First commit

---
+++ src/main/java/com/example/forestofhabits/ForestofhabitsApplication.java
@@ -0,0 +1,13 @@
+package com.example.forestofhabits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForestofhabitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForestofhabitsApplication.class, args);
+	}
+
+}
